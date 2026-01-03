55ccfe615c1b37fa8e3fef633bb55068fec5e0be
Aaron K <aka.signups@gmail.com>
2026-01-03 14:11:55 +1100

    testing creating new folder on branch

--- blackjack-game.js
+++ blackjack-game.js
@@ -1,4 +1,4 @@
-// ----- Card / Deck helpers -----
+// ----- Card / Deck helpers -----pushchange
 const SUITS = ["♠", "♥", "♦", "♣"];
 const RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
